8bcc8e0f171ad8b4782feccfcc46c27b5a258d9a
Sai-Charan-Bandari <bsaicharan30@gmail.com>
2023-12-04 23:21:23 +0530

    Added events to initiate rematch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 function Home({io}) {
     const [name, setName] = useState('')
     const [frndName, setFrndName] = useState('')
-    const [loader, setLoader] = useState('')
+    const [loader, setLoader] = useState('connecting to server...')
     const [myAction, setMyAction] = useState(!1)
     // showActions is used to show the values that the players enter during each delivery
     const [showActions, setShowActions] = useState('') //{user : 0, frnd : 0} obj format
     const [myToss, setMyToss] = useState(!1)
-    const [stage, setStage] = useState(-1)
+    const [stage, setStage] = useState(-10)
     const [scores, setScores] = useState({ myScore: -1, frndScore: -1 })
   
     
@@ -22,9 +22,18 @@ function Home({io}) {
     }
     
     let reset=()=>{
+      console.log('game over, reset called')
         setStage(-2)
         // it can be set when u click on play again
         // setScores({myScore:-1,frndScore:-1}) 
+        setLoader('')
+        setShowActions('')
+    }
+    
+    let reset2=()=>{
+      setFrndName('')
+        setStage(-1)
+        setScores({myScore:-1,frndScore:-1}) 
         setLoader('')
         setShowActions('')
     }
@@ -32,25 +41,85 @@ function Home({io}) {
     useEffect(()=>{
 
    io.on('connect', () => {
-     // alert('connected to server')
-     console.log('connected to server')
+     //connection to sever has been established
+     setStage(-1) 
+     setLoader('')
      console.log('my id is ',io.id)
+     console.log('connected to server')
+   })
+
+   io.on('disconnect', () => {
+     console.log('disconnected to server')
+   })
+
+   io.on('opponent_disconnected', () => {
+     console.log('it seems the opponent disconnected')
+    //  alert('it seems the opponent disconnected')
+     reset2()
    })
      
       io.on('hello',(k)=>{
         console.log('hello ',name,'&',frndName)
       })
+     
+      io.on('in_game',(k)=>{
+        console.log('player already joined a game')
+        alert('player already joined a game')
+      })
       
       io.on('play_req', (k) => {
-        setFrndName(k)
-        if (confirm(k+' has invited u for a match')) {
-          let n=name
-          while(n==''){
-             n = prompt('plz enter ur name first','')
-            setName(n)
+        if(stage==-1){
+          setFrndName(k.name)
+          if (confirm(k.name+' has invited u for a match')) {
+            let n=name
+            while(n==''){
+               n = prompt('plz enter ur name first','')
+              setName(n)
+            }
+            io.emit('play_ack',{senderId:k.id, name:n})
+            setLoader('waiting for '+k.name+ '\'s toss selection')
+            setStage(0)
           }
-          io.emit('play_ack', n)
-          setLoader('waiting for '+k+ '\'s toss selection')
+        }else{     
+           // i am already in a game, cant join
+        }
+      })
+      
+      io.on('play_ack', (k) => {
+        if(stage==-3){
+          setFrndName(k)
+          setLoader('')
+          let t = confirm('odd or even ?')
+          // odd -> true, even -> false
+          io.emit('toss', t)
+          setMyToss(t)
+          setStage(1)
+        }else{      
+          // i am already in a game, cant join
+        }
+      })
+      
+      io.on('rematch_req', (k) => {
+        if(stage==-2){
+          setLoader(frndName+' has invited u for a re-match. Click play_again to start match')
+        }
+      })
+      
+      io.on('rematch_started_toss',s=>{
+        console.log('rematch toss')
+        if(stage==-3){
+          let t = confirm('odd or even ?')
+          // odd -> true, even -> false
+          io.emit('toss', t)
+          setStage(1)
+          setLoader('')
+            setMyToss(t)
+        }
+      })
+      
+      io.on('rematch_started_wait',s=>{
+        if(stage==-3){
+          setLoader('waiting for '+frndName+ '\'s toss selection')
           setStage(0)
         }
       })
@@ -62,16 +131,6 @@ function Home({io}) {
           setStage(1)
           setLoader('')
         }
-      })
-  
-      io.on('play_ack', (k) => {
-        setFrndName(k)
-        setLoader('')
-        let t = confirm('odd or even ?')
-        // odd -> true, even -> false
-        io.emit('toss', t)
-        setMyToss(t)
-        setStage(1)
       })
   
       io.on('toss_results', (t) => {
@@ -118,12 +177,17 @@ function Home({io}) {
           if(t.bat == t.bowl){ //out
             setLoader('OUT! OH NO.')
             if(scores.frndScore == -1){
-              setTimeout(()=>{setLoader('Changing innings...');setTimeout(setLoader,1000,'')}, 1500);
-              // change innings
+              setTimeout(()=>{
+                setLoader('Changing innings...');
+                setTimeout(setLoader,1000,'')
+              }, 1500);
+              setTimeout(() => {
+                // change innings
               io.emit('action',!myAction)
               setMyAction(!myAction)
               setScores({...scores,frndScore:0})
               setStage(2)
+              }, 2500);
             }else{
               // declare win,loss,tie
               console.log('need to declare ')
@@ -134,25 +198,27 @@ function Home({io}) {
               }else{
                 setLoader('its a tie')
               }
+              // io.emit('game_over','')
               setTimeout(reset,2000)
             }
           }else{ //not out, i scored some runs 
             if((scores.frndScore != -1) && (scores.myScore+t.bat > scores.frndScore)){
               //win
               setLoader('yayy u won')
+              // io.emit('game_over','')
               setTimeout(reset,2000)
             }else{
               setLoader('')
+              setStage(2)
             }
             setScores({...scores, myScore:scores.myScore+t.bat})
-            setStage(2)
           }
         }else{ //bowling
           setShowActions({user : t.bowl, frnd : t.bat})
           if(t.bat == t.bowl){ //out
             setLoader('OUT! YESS.')
             if(scores.myScore == -1){
-              setTimeout(()=>{setLoader('Changing innings...');setTimeout(setLoader,1000,'')}, 1500);
+              setTimeout(()=>{setLoader('Changing innings...')}, 1500);
               // change innings action need not be toggled or emitted by u bcoz it will be already emitted from the batting side
               // u will receive the action and set it
               // io.emit('action',!myAction)
@@ -167,18 +233,20 @@ function Home({io}) {
               }else{
                 setLoader('its a tie')
               }
+              // io.emit('game_over','')
               setTimeout(reset,2000)
             }
           }else{ //not out , ur frnd scored some runs
               if((scores.myScore != -1) && (scores.myScore < scores.frndScore+t.bat)){
               //lost
               setLoader('oops u lost')
+              // io.emit('game_over','')
               setTimeout(reset,2000)
             }else{
               setLoader('')
+              setStage(2)
             }
             setScores({...scores, frndScore:scores.frndScore+t.bat})
-            setStage(2)
           }
         }
         
@@ -206,20 +274,24 @@ function Home({io}) {
              n = prompt('plz enter ur name first','')
             setName(n)
           }
-          io.emit('play_req', n)
-          setStage(0)
+          io.emit('play_req',n)
+          setStage(-3) //indicates that i am waiting for someone to respond/join
           setLoader('Waiting for a frnd to accept ur request')
         }}>play</button>
         </div>}
   
         {stage == -2 && <div>
-        <input type="text" placeholder='enter ur name' value={name} onChange={(e)=>setName(e.target.value)} />
+        {/* <input type="text" placeholder='enter ur name' value={name} onChange={(e)=>setName(e.target.value)} /> */}
           <button onClick={() => {
             setScores({myScore:-1, frndScore:-1})
-          io.emit('play_req', name)
-          setStage(0)
-          setLoader('Waiting for a frnd to accept ur request')
+          io.emit('rematch_req', name)
+          setStage(-3) //indicates that i am waiting for frnd to respond/join
+          setLoader('Waiting for ' +frndName+' to accept ur request')
         }}>play again</button>
+        <button onClick={()=>{
+          io.emit('no_rematch','')
+          reset2()
+        }}>exit</button>
         </div>}
   
         {loader!='' && <div>
@@ -235,7 +307,7 @@ function Home({io}) {
   
         {(scores.myScore!=-1 || scores.frndScore!=-1) && <div>
           <h3>Innings :</h3>
-          <div>{name} : {myAction ? 'batting' : 'bowling'} , {scores.myScore!=-1 && `score : ${scores.myScore}`}</div>
+          <div>{name} (You) : {myAction ? 'batting' : 'bowling'} , {scores.myScore!=-1 && `score : ${scores.myScore}`}</div>
           <div>{frndName} : {!myAction ? 'batting' : 'bowling'} , {scores.frndScore!=-1 && `score : ${scores.frndScore}`}</div>
         </div>}
   
